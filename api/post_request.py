3da0aa724649f103ad46105e9170cb82b51537bf
Sudhanva <nsudhanva@gmail.com>
2018-08-17 13:49:19 +0530

    Flask API and requests

---
+++ api/post_request.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+req_rec = requests.post("http://127.0.0.1:5000/recommend", params={'user_id': 35})
+req_rel = requests.post("http://127.0.0.1:5000/related", params={'assignment_id': 10})
+
+print(req_rec.status_code, req_rec.reason)
+print(req_rel.status_code, req_rel.reason)
+
+# json_data_rec = json.loads(req_rec.text)
+# json_data_rel = json.loads(req_rel.text)
+
+# print(json_data_rec)
+# print(json_data_rel)
