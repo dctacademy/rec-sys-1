74bc20145037e9f7b7d9d9f942661404dd28530a
Sudhanva Narayana <nsudhanva@gmail.com>
2018-12-18 18:39:08 +0530

    Update Models 18-12-18

--- nlp/user_37.js
+++ nlp/user_37.js
@@ -38,7 +38,36 @@ class StringWeight {
     }
 }
 
-function strWeight(x, y) {
-    let w1 = new StringWeight(x, y);
-    return w1.stringWeight();
+function strWeight(a,b){
+	let w1 = new Weight(a,b);
+	return w1.compare();
+}class Weight {
+	constructor(a,b) {
+		this.str = a;
+		this.str1 = b
+	}
+	compare() {
+		let sum = 0;
+		let sum1 =0;
+		const alp = 'abcdefghijklmnopqrstuvwxyz';
+		for(var i=0;i<alp.length;i++) {
+			for(var j =0;j<this.str.length;j++) {
+				if(this.str[j] === alp[i]) {
+					sum += i+1;
+				}
+			};
+		 for(var k=0; k<this.str1.length;k++) {
+			if(this.str1[k] == alp[i]){
+				sum1 += i+1;
+			}
+		};
+			if(sum > sum1){
+				return 2;
+			} else if(sum < sum1) {
+				return 1;
+			} else {
+				return 'equal';
+			}
+		}
+	}
 }
